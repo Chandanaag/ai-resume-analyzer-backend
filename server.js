c57962c646b137e9af53e22b2dd7d5cc00eb2681
3Tarun <tarun.h@rovelabs.com>
2026-03-09 00:50:49 +0530

    changes made in server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
